58935c3b73f31f0899e345994cc2bcca6430f87a
coco0412 <co860628@gmail.com>
2025-01-24 18:08:11 -0700

    n

---
+++ OOPFun/OOPFun/FoodItem.cs
@@ -0,0 +1,22 @@
+namespace OOPFun;
+
+class FoodItem
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Quantity { get; set; }
+    public DateTime ExpirationDate { get; set; }
+
+    public FoodItem(string name, string category, int quantity, DateTime expirationDate)
+    {
+        Name = name;
+        Category = category;
+        Quantity = quantity >= 0 ? quantity : throw new ArgumentException("Quantity cannot be negative.");
+        ExpirationDate = expirationDate;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} | {Category} | Quantity: {Quantity} | Expiration Date: {ExpirationDate.ToShortDateString()}";
+    }
+}
